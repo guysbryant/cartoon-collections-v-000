485919a9e1e66934d315e1e11e65d99e1c86417e
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-15 02:21:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,11 @@
 def roll_call_dwarves(list)# code an argument here
   # Your code here
-  list.each_with_index {|name, num| puts num name
+  list.each_with_index {|name, num| puts num name}
 end
 
 def summon_captain_planet(list)# code an argument here
   # Your code here
-  list.collect {|element| "#{element.titlecase}!"
+  list.collect {|element| "#{element.titlecase}!"}
 end
 
 def long_planeteer_calls(list)# code an argument here
